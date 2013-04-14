38c670b425a49502dddfc4632f9ce35311a73fba
Robert Krause <robert.allen.krause@gmail.com>
2013-04-13 21:13:37 -0500

    - Made the command handling take care of non-existing ones.

--- user-commands.lisp
+++ user-commands.lisp
@@ -17,13 +17,18 @@
   (lambda (msg)
     (progn
       (let* ((cmd (first-word (cadr (irc::arguments msg))))
-	     (cmd-name (subseq cmd 1)))
-	(if (and (char= (char cmd 0) #\^)
-		 (load (format nil "user-commands/~a.lisp" cmd-name))
-		 (gethash cmd-name *registered-commands*))
-	    (funcall (gethash cmd-name *registered-commands*) msg connection)
-	  (format T "~a Not a registered command" cmd)))
-	(princ ""))))
+	     (cmd-name (subseq cmd 1))
+	     (cmd-file-name (format nil "user-commands/~a.lisp" cmd-name)))
+	(if (char= (char cmd 0) #\^)
+	    (if (and (probe-file cmd-file-name)
+		     (load cmd-file-name)
+		     (gethash cmd-name *registered-commands*))
+		(funcall (gethash cmd-name *registered-commands*) msg connection)
+	      (progn 
+		(princ (cadr (irc::arguments msg)))
+		(irc:privmsg connection (irc:source msg)
+			     (format nil "~a is not a valid command" cmd-name))))
+	  (princ ""))))))
 
 ;; this will walk the .lisp files in user-commands/
 ;; it should register each file it finds with a hash map.

9c81fbcc368473e1d7ce89e47e0a3c06cb8ac063
Robert Krause <robert.allen.krause@gmail.com>
2013-05-02 23:31:40 -0500

    - added lod and poop

---
+++ user-commands/poop.lisp
@@ -0,0 +1,23 @@
+;; Copyright 2013 Robert Allen Krause <robert.allen.krause@gmail.com>
+
+;;     This file is part of Robort.
+
+;;     Robort is free software: you can redistribute it and/or modify
+;;     it under the terms of the GNU General Public License as published by
+;;     the Free Software Foundation, either version 3 of the License, or
+;;     (at your option) any later version.
+
+;;     Robort is distributed in the hope that it will be useful,
+;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;     GNU General Public License for more details.
+
+;;     You should have received a copy of the GNU General Public License
+;;     along with Robort.  If not, see <http://www.gnu.org/licenses/>.
+(in-package :user-commands)
+
+(load "user-commands/common.lisp")
+
+(defun poop (msg connection)
+    (irc:privmsg connection (get-destination msg) "💩"))
+(export 'poop)

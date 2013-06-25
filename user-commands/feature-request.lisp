a3b3a9e272fae712679556e077fcec3dbd86471b
Robert Allen Krause <robert.allen.krause@gmail.com>
2013-06-25 09:59:40 -0500

    - added feature request for funzies

---
+++ user-commands/feature-request.lisp
@@ -0,0 +1,26 @@
+;; Copyright 2013 Robert Allen Krause <robert.allen.krause@gmail.com>
+
+;;     This file is part of Robort.
+
+;;     Robort is free software: you can redistribute it and/or modify
+;;     it under the terms of the GNU General Public License as published by
+;;     the Free Software Foundation, either version 3 of the License, or
+;;     (at your option) any later version.
+
+;;     Robort is distributed in the hope that it will be useful,
+;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;     GNU General Public License for more details.
+
+;;     You should have received a copy of the GNU General Public License
+;;     along with Robort.  If not, see <http://www.gnu.org/licenses/>.
+(require :cl-irc)
+
+(in-package :user-commands)
+
+(load "user-commands/common.lisp")
+
+(let ((feature-request-message "Check out ^source. GitHub supports filing issues. If you're feeling adventurous, pull requests are preferred. My creator learned Common Lisp and so can you!"))
+  (defun feature-request (msg connection)
+    (irc:privmsg connection (get-destination msg) feature-request-message)))
+(export 'feature-request)
